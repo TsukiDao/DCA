12cc2f4aa969b49676234295433ad6cb3eb60894
Icebergy <icebergy@protonmail.com>
2022-01-20 17:47:43 -0600

    Update vault.sol

--- vault.sol
+++ vault.sol
@@ -1,3 +1,34 @@
+pragma solidity >= 0.5 < 0.8;
+
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol'; // ERC20 Interface
 import './uniswapInterfaceV1.sol';
 import "./SafeMath.sol";
+
+// need erc interfance and correct contract panacksawp
+
+
+contract DCA {
+
+    strut 
+
+    function deposit {
+    
+    }
+    
+    
+    function withdraw {
+    
+    }
+    
+    function setupDCA {
+    
+    }
+    
+    function swap {
+    
+    }
+    
+
+
+
+}
