d3e90bc0361aafbe5a28f1c141f934e557be005a
Icebergy <icebergy@protonmail.com>
2022-01-20 17:45:05 -0600

    Update vault.sol

--- vault.sol
+++ vault.sol
@@ -1,1 +1,3 @@
-
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol'; // ERC20 Interface
+import './uniswapInterfaceV1.sol';
+import "./SafeMath.sol";
